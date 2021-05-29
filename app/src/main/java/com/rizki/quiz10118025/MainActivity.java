3daad0fe6a835e63887bd2e756f55c0fd66f89f0
resturr <restur2411@gmail.com>
2021-05-29 12:16:53 +0700

    Beres

---
+++ app/src/main/java/com/rizki/quiz10118025/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rizki.quiz10118025;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// 29 Mei 2021 10118025 Rizki Restu Illahi IF-1
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
